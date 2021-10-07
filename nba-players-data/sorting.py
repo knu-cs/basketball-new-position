8feb80fb8d9cc62d39e6928d8c8ac94bc8639701
SHIN BEOMSOO <sdalbsoo@gmail.com>
2021-10-07 22:36:37 +0900

    player career column add height, weight

--- nba-players-data/sorting.py
+++ nba-players-data/sorting.py
@@ -19,9 +19,15 @@ def get_nba_player_info(player_id: str) -> DataFrame:
     player = commonplayerinfo.CommonPlayerInfo(player_id=player_id)
     return player.common_player_info.get_data_frame()
 
-# all_players = get_all_nba_players()
-# player_career = get_nba_career_stats_by_id(all_players[0]['id'])
-# player_info = get_nba_player_info(all_players[2]['id'])
-# player_career.insert(1, "HEIGHT", player_info.get("HEIGHT")[0])
-# player_career.insert(2, "WEIGHT", player_info.get("WEIGHT")[0])
-# print(player_career)
+
+all_players = get_all_nba_players()
+count = 0
+for player in all_players:
+    player_career = get_nba_career_stats_by_id(player["id"])
+    player_info = get_nba_player_info(player["id"])
+    player_career.insert(1, "HEIGHT", player_info.get("HEIGHT")[0])
+    player_career.insert(2, "WEIGHT", player_info.get("WEIGHT")[0])
+    print(player_career)
+    count += 1
+    if count == 7:
+        break
